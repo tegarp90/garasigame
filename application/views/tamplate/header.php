60468190da629fd4e379e03ffccba4ba08eed6b3
iyelbrq <msuhail2711@gmail.com>
2021-01-11 05:43:56 +0700

    google auth composer

--- application/views/tamplate/header.php
+++ application/views/tamplate/header.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Wicodus - Home</title>
+    <title>GarasiGame</title>
 
     <!-- CSS -->
     <link href="<?php echo base_url();?>assets/css/fonts/etline-font.min.css" rel="stylesheet">
