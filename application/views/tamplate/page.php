1867a5051d2f66b70ecff12308ce03c7bc72749a
iyelbrq <msuhail2711@gmail.com>
2021-01-13 01:56:15 +0700

    update 20210113

--- application/views/tamplate/page.php
+++ application/views/tamplate/page.php
@@ -1,9 +1,10 @@
 <?php
-	$this->load->view('modal/modal_sign');
+	
     $this->load->view('tamplate/header');
     $this->load->view('tamplate/navbar');
     if(@$content!='')
     {
     $this->load->view(@$content);
     }
+    $this->load->view('modal/modal_sign');
 ?>
